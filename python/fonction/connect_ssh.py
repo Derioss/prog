c131ccb4af254fe69708181ecb29a16b0cb66e9c
derios <maury.frederic@gmail.com>
2018-09-01 20:13:13 +0200

    function connect ssh

---
+++ python/fonction/connect_ssh.py
@@ -0,0 +1,16 @@
+def nouvelle_co():
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect('***', username='***', password='***')
+    return client
+
+def test():
+    client = nouvelle_co()
+    #stdin, stdout, stderr = client.exec_command("ls")
+    command = ls
+    stdin, stdout, stderr = client.exec_command(command)
+    #stdin, stdout, stderr = client.exec_command(command2)
+    for line in stdout.read().splitlines():
+        print(line)
+    client.close()
+    return
