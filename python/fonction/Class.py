f8029a22e066e7d58fb6adc98d27cb8d419a7bd0
Derioss <44114185+Derioss@users.noreply.github.com>
2018-12-03 09:40:24 +0100

    Update Class.py
    debug Rename.purge()

--- python/fonction/Class.py
+++ python/fonction/Class.py
@@ -25,11 +25,10 @@ class Rename :
                 z = ZipFile(path)
                 z.extractall(path=path[:-4])
 
-    def purge(dir, pattern):
-        for f in os.listdir(dir):
-            if re.search(pattern, f):
-                os.remove(os.path.join(dir, f))
-
+  def purge(self):
+        for f in os.listdir(self.dir):
+            if re.search(self.pattern, f):
+                os.remove(os.path.join(self.dir, f))
 
 
 class Path:
